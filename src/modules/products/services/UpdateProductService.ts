0a4a0b5f44103beb4972bf48b89666b479a70523
Vinicius <vinicius.devjvm@gmail.com>
2023-09-02 19:29:13 -0300

    criado o servico de atualizacao do produto

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,38 @@
+import { ServerApiVersion, getCustomRepository } from "typeorm";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepositoy";
+import Product from "../typeorm/entities/Product";
+import AppError from '../../../shared/errors/AppError';
+
+interface IRequest{
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class ShowProductService {
+  public async execute({id,name,price,quantity}: IRequest): Promise<Product>{
+    const productRepositorys = getCustomRepository(ProductRepository);
+    const product = await productRepositorys.findOneById(id);
+
+    if(!product){
+      throw new AppError("Product Not found");
+    }
+
+    const productExists = await productRepositorys.findByName(name);
+    
+    if(productExists !== undefined){
+      throw new AppError("There is already one product with this name");
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productRepositorys.save(product);
+    
+    return product;
+  }
+}
+
+export default ShowProductService;
