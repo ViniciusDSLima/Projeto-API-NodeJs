0dce1a22d937fa786c817683c40d590ed22784a5
Vinicius <vinicius.devjvm@gmail.com>
2023-09-02 20:50:42 -0300

    criadas as rotas da aplicacao e exportadas para a classe index

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,10 @@
+import productsRouter from "@modules/products/routes/products.roules";
 import { Router } from "express";
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({message: "Hello dev!" });
-});
+routes.use("/products", productsRouter);
+
 
 
 export default routes;
