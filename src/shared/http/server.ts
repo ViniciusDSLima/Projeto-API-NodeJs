81a93ec500570612699970ac0a4c315b4a13c800
Vinicius <vinicius.devjvm@gmail.com>
2023-09-02 22:02:53 -0300

    criadas os tratamentos dos parametros para o CRUD do produto.

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,20 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from "express";
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from "@shared/errors/AppError";
 import '@shared/typeorm';
+import { errors } from 'celebrate';
+import { error } from 'console';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
 app.use(
   (
     error : Error,
