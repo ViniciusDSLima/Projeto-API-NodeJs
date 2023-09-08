641a7cd08f6201f99c45c5997974b969e3fb7b04
Vinicius <vinicius.devjvm@gmail.com>
2023-09-07 22:45:34 -0300

    Servico de redefinicao de senha totalmente criado.

--- src/modules/Users/services/SendForgotPasswordEmailService.ts
+++ src/modules/Users/services/SendForgotPasswordEmailService.ts
@@ -18,7 +18,6 @@ class SendForgotPasswordEmailService {
     if(!user){
         throw new AppError("User does not exists!");
     }
-
     const token = await userTokenRepository.generate(user.id);
     
     console.log(token);
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,7 +1,9 @@
 import AuthController from "@modules/Users/controllers/AuthController";
 import authRouter from "@modules/Users/routes/Auth.routes";
 import usersRouter from "@modules/Users/routes/Users.routes";
+import passwordRouter from "@modules/Users/routes/password.route";
 import productsRouter from "@modules/products/routes/products.roules";
+import { ro } from "date-fns/locale";
 import { Router } from "express";
 
 const routes = Router();
@@ -9,5 +11,6 @@ const routes = Router();
 routes.use("/products", productsRouter);
 routes.use("/users", usersRouter);
 routes.use("/auth", authRouter);
+routes.use("/password", passwordRouter);
 
 export default routes;
