5e1d37715b815fe1c751bde655eb4eeeedc70d24
Vinicius <vinicius.devjvm@gmail.com>
2023-09-07 22:48:35 -0300

    criado o ResetPassowoirdController

---
+++ src/modules/Users/controllers/ResetPasswordController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import ListUserService from "../services/ListUserService";
+import CreateUserService from "../services/CreateUserService";
+import SendForgotPasswordEmailService from "../services/SendForgotPasswordEmailService";
+import ResetPasswordService from "../services/ResetPasswordService";
+
+export default class ResetPasswordController{
+    public async create(req: Request, res: Response): Promise<Response>{
+        const { password, token}= req.body;
+
+
+        const resetPassword  = new ResetPasswordService();
+
+        await resetPassword.execute({
+            password,
+            token
+        });
+
+        return res.status(204).json();
+    }
+}
