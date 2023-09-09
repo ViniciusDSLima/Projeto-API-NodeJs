9902c5995ba87cc237826f29da68f41d1a3d2317
Vinicius <vinicius.devjvm@gmail.com>
2023-09-09 12:36:56 -0300

    Criado o servico de listagem do perfil do usuario

---
+++ src/modules/Users/services/ShowProfileService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/Users";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+import { getRandomValues } from "crypto";
+import AppError from "@shared/errors/AppError";
+
+interface IRequest {
+  user_id: string;
+}
+
+
+class ShowProfileService {
+    public async execute({user_id}: IRequest): Promise<User>{
+      const usersRepository = getCustomRepository(UsersRepository);
+
+      const user = await usersRepository.findById(user_id);
+      
+      if(!user){
+        throw new AppError("User not found");
+      }
+
+      return user;
+    }
+  }
+  
+  export default ShowProfileService;
