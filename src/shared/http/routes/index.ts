7f8d911b8b4808a1afac76d0d1b0c08f94b2791f
Vinicius <vinicius.devjvm@gmail.com>
2023-09-09 14:07:24 -0300

    Criadas as rotas para o Customers

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import authRouter from "@modules/Users/routes/Auth.routes";
 import profileRouter from "@modules/Users/routes/Profile.routes";
 import usersRouter from "@modules/Users/routes/Users.routes";
 import passwordRouter from "@modules/Users/routes/password.route";
+import customerRouter from "@modules/customers/routes/customers.routes";
 import productsRouter from "@modules/products/routes/products.roules";
 import { ro } from "date-fns/locale";
 import { Router } from "express";
@@ -14,5 +15,6 @@ routes.use("/users", usersRouter);
 routes.use("/auth", authRouter);
 routes.use("/password", passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customerRouter);
 
 export default routes;
