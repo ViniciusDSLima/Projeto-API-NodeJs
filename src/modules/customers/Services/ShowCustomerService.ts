ed631941323e66798825e2d37f12a6ab8273c8db
Vinicius <vinicius.devjvm@gmail.com>
2023-09-09 13:40:07 -0300

    Criado todos os servicos dos customers

---
+++ src/modules/customers/Services/ShowCustomerService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/AppError";
+import Customer from "../typeorm/entities/Customer";
+import CustomersRepository from "../typeorm/repositories/CustomersRepository";
+import { custom } from "joi";
+
+interface IRequest {
+  id: string;
+}
+
+
+class ShowCustomer {
+    public async execute({id}: IRequest): Promise<Customer>{
+      const customerRepository = getCustomRepository(CustomersRepository);
+
+      const customer = await customerRepository.findById(id);
+      
+      if(!customer){
+        throw new AppError("User not found");
+      }
+
+      return customer;
+    }
+  }
+  
+  export default ShowCustomer;
