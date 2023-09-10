286f42655d0ca47d2bc6245d8e97b893abbb6d3d
Vinicius <vinicius.devjvm@gmail.com>
2023-09-10 12:54:56 -0300

    Criado o OrderController

---
+++ src/modules/orders/controller/OrderController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { promises } from 'dns';
+import { Repository } from 'typeorm';
+import { STATUS_CODES } from "http";
+import ShowOrderService from "../services/ShowOrderService";
+import CreateOrderService from "../services/CreateOrderService";
+
+
+export default class OrdersController{
+
+  public async show(req: Request, res: Response): Promise<Response>{
+    const {id} = req.params;
+
+    const showOrder = new ShowOrderService();
+
+    const order = await showOrder.execute({id});
+
+    return res.json(order);
+  }
+
+  public async create(req:Request, res: Response): Promise<Response>{
+    const {customer_id, products} = req.body;
+
+    const createOrder  = new CreateOrderService();
+
+    const order = await createOrder.execute({
+      customer_id, products
+    });
+
+
+    return res.status(201).json(order);
+  }
+
+}
