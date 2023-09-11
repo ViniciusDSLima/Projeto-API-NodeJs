177f2770a7d1596df4c458dae42ca7c7e20fc92e
Vinicius <vinicius.devjvm@gmail.com>
2023-09-11 11:00:23 -0300

    Reconfigurada a classe RedisCache

--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -1,8 +1,5 @@
 import Redis, { Redis as RedisClient } from 'ioredis';
 import cacheConfig from '@config/cache';
-import { promises } from 'dns';
-import { da } from 'date-fns/locale';
-import { parentPort } from 'worker_threads';
 
 export default class RedisCache{
     private client: RedisClient;
