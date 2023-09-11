acee0df1c248739f41635232e8ea0d57c4112c9e
Vinicius <vinicius.devjvm@gmail.com>
2023-09-11 09:50:36 -0300

    Inicializacao e configuracao do Redis na aplicaco

---
+++ src/config/cache.ts
@@ -0,0 +1,21 @@
+import { ReadPreference } from "typeorm";
+import { RedisOptions } from "ioredis";
+
+interface IChacheConfig{
+    config: {
+        redis: RedisOptions;
+    };
+    driver: string;
+}
+
+
+export default {
+    config: {
+        redis: {
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+            password: process.env.REDIS_PASS ||  undefined,
+        },
+    },
+    driver: 'redis',
+} as IChacheConfig;
